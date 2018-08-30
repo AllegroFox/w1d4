8030f80ada6c36e37b61d86db79ffe6e2ff78416
Emily McMinn <emily@mcminn.ca>
2018-08-30 22:57:57 +0000

    implemented the wrapLog function for the stretch assignment.

---
+++ logging_wrapper_stretch.js
@@ -0,0 +1,33 @@
+var wrapLog = function (callback, name) {
+  return function() {
+    var results = callback.apply(null, arguments);
+    var params = Array.from(arguments);
+
+    console.log(`${name}(${params}) => ${results}`);
+  }
+};
+
+
+var double = function(number) {
+  return (number * 2);
+};
+
+var logDouble = wrapLog(double, "double");
+
+logDouble(4); //double(4) => 8
+
+var area = function (x, y) {
+  return x * y;
+};
+var logArea = wrapLog(area, "area");
+
+logArea(5, 3); // area(5, 3) => 15
+logArea(3, 2); // area(3, 2) => 6
+
+var volume = function (x, y, z) {
+  return x * y * z;
+};
+var logVolume = wrapLog(volume, "volume");
+
+logVolume(5, 3, 2); // volume(5, 3, 2) => 30
+logVolume(3, 2, 4); // volume(3, 2, 4) => 24
